00343a1e9ab533b3ec3ce0ea19839a6ceb78ef3e
Maxime <52084503+Max-3-7@users.noreply.github.com>
2023-09-11 10:56:47 +0200

    add more tests

--- packages/v2/src/utils/liquidityDistribution.test.ts
+++ packages/v2/src/utils/liquidityDistribution.test.ts
@@ -14,7 +14,7 @@ const token0 = new Token(
   't0'
 )
 
-it('getUniformDistributionFromBinRange works', () => {
+it('getUniformDistributionFromBinRange', () => {
   const activeId = 8376038
 
   const params = getUniformDistributionFromBinRange(activeId, [
@@ -62,7 +62,7 @@ it('getUniformDistributionFromBinRange works', () => {
   expect(sumDistributionY).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
 })
 
-it('getBidAskDistributionFromBinRange works', () => {
+it('getBidAskDistributionFromBinRange with amount X and Y', () => {
   const activeId = 8376038
   const amountA = CurrencyAmount.ether(43113, parseEther('1'))
   const amountB = new TokenAmount(token0, parseEther('0.5'))
@@ -113,7 +113,79 @@ it('getBidAskDistributionFromBinRange works', () => {
   // expect(sumDistributionY).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
 })
 
-it('getCurveDistributionFromBinRange works', () => {
+it('getBidAskDistributionFromBinRange with amount X only', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('1'))
+  const amountB = new TokenAmount(token0, parseEther('0'))
+
+  const params = getBidAskDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionX = params.distributionX.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([0, 1, 2, 3, 4, 5])
+  expect(params.distributionX).toEqual([
+    BigInt('47619047619047619'),
+    BigInt('95238095238095238'),
+    BigInt('142857142857142857'),
+    BigInt('190476190476190476'),
+    BigInt('238095238095238095'),
+    BigInt('285714285714285714')
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0)
+  ])
+  expect(sumDistributionX).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getBidAskDistributionFromBinRange with amount Y only', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('0'))
+  const amountB = new TokenAmount(token0, parseEther('0.5'))
+
+  const params = getBidAskDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionY = params.distributionY.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([-5, -4, -3, -2, -1, 0])
+  expect(params.distributionX).toEqual([
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0)
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt('285714285714285714'),
+    BigInt('238095238095238095'),
+    BigInt('190476190476190476'),
+    BigInt('142857142857142857'),
+    BigInt('95238095238095238'),
+    BigInt('47619047619047619')
+  ])
+  expect(sumDistributionY).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getCurveDistributionFromBinRange with amount X and Y', () => {
   const activeId = 8376038
   const amountA = CurrencyAmount.ether(43113, parseEther('1'))
   const amountB = new TokenAmount(token0, parseEther('0.5'))
@@ -162,4 +234,76 @@ it('getCurveDistributionFromBinRange works', () => {
   ])
   // expect(sumDistributionX).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
   // expect(sumDistributionY).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getCurveDistributionFromBinRange with amount X only', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('1'))
+  const amountB = new TokenAmount(token0, parseEther('0'))
+
+  const params = getCurveDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionX = params.distributionX.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([0, 1, 2, 3, 4, 5])
+  expect(params.distributionX).toEqual([
+    BigInt('244436099596057025'),
+    BigInt('232006448180761579'),
+    BigInt('198382877939128516'),
+    BigInt('152819135187004190'),
+    BigInt('106052450888545820'),
+    BigInt('66302988208502866')
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0')
+  ])
+  // expect(sumDistributionX).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getCurveDistributionFromBinRange with amount Y only', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('0'))
+  const amountB = new TokenAmount(token0, parseEther('0.5'))
+
+  const params = getCurveDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionY = params.distributionY.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([-5, -4, -3, -2, -1, 0])
+  expect(params.distributionX).toEqual([
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0')
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt('66302988208502866'),
+    BigInt('106052450888545820'),
+    BigInt('152819135187004190'),
+    BigInt('198382877939128516'),
+    BigInt('232006448180761579'),
+    BigInt('244436099596057025')
+  ])
+  expect(sumDistributionY).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
 })
