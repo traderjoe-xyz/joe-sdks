75d84d4d5961294497ef316840202b2a8858ef1f
Maxime <52084503+Max-3-7@users.noreply.github.com>
2023-09-12 10:25:15 +0200

    Rewrite get distribution params functions (#18)
    * rewrite getBidAskDistributionFromBinRange
    
    * rewrite getCurveDistributionFromBinRange
    
    * add more tests
    
    * cleanup
    
    * add changeset

--- packages/v2/src/utils/liquidityDistribution.test.ts
+++ packages/v2/src/utils/liquidityDistribution.test.ts
@@ -1,52 +1,309 @@
-import { describe, it, expect } from 'vitest'
-import { getUniformDistributionFromBinRange } from './liquidityDistribution'
+import { it, expect } from 'vitest'
+import {
+  getUniformDistributionFromBinRange,
+  getBidAskDistributionFromBinRange,
+  getCurveDistributionFromBinRange
+} from './liquidityDistribution'
+import { CurrencyAmount, Token, TokenAmount } from '@traderjoe-xyz/sdk-core'
+import { parseEther } from 'viem'
 
-describe('getUniformDistributionFromBinRange', () => {
-  it('computes delta ids and distribution values correctly', () => {
-    const activeId = 8376038
+const token0 = new Token(
+  43113,
+  '0x0000000000000000000000000000000000000001',
+  18,
+  't0'
+)
 
-    const params = getUniformDistributionFromBinRange(activeId, [
-      activeId - 5,
-      activeId + 5
-    ])
+it('getUniformDistributionFromBinRange', () => {
+  const activeId = 8376038
 
-    const sumDistributionX = params.distributionX.reduce(
-      (a, b) => a + b,
-      BigInt(0)
-    )
-    const sumDistributionY = params.distributionY.reduce(
-      (a, b) => a + b,
-      BigInt(0)
-    )
+  const params = getUniformDistributionFromBinRange(activeId, [
+    activeId - 5,
+    activeId + 5
+  ])
 
-    expect(params.deltaIds).toEqual([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
-    expect(params.distributionX).toEqual([
-      BigInt(0),
-      BigInt(0),
-      BigInt(0),
-      BigInt(0),
-      BigInt(0),
-      BigInt('90909090909090909'),
-      BigInt('181818181818181818'),
-      BigInt('181818181818181818'),
-      BigInt('181818181818181818'),
-      BigInt('181818181818181818'),
-      BigInt('181818181818181818')
-    ])
-    expect(params.distributionY).toEqual([
-      BigInt('181818181818181818'),
-      BigInt('181818181818181818'),
-      BigInt('181818181818181818'),
-      BigInt('181818181818181818'),
-      BigInt('181818181818181818'),
-      BigInt('90909090909090909'),
-      BigInt(0),
-      BigInt(0),
-      BigInt(0),
-      BigInt(0),
-      BigInt(0)
-    ])
-    expect(sumDistributionX).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
-    expect(sumDistributionY).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
-  })
+  const sumDistributionX = params.distributionX.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+  const sumDistributionY = params.distributionY.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+  expect(params.distributionX).toEqual([
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt('90909090909090909'),
+    BigInt('181818181818181818'),
+    BigInt('181818181818181818'),
+    BigInt('181818181818181818'),
+    BigInt('181818181818181818'),
+    BigInt('181818181818181818')
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt('181818181818181818'),
+    BigInt('181818181818181818'),
+    BigInt('181818181818181818'),
+    BigInt('181818181818181818'),
+    BigInt('181818181818181818'),
+    BigInt('90909090909090909'),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0)
+  ])
+  expect(sumDistributionX).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
+  expect(sumDistributionY).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getBidAskDistributionFromBinRange with amount X and Y', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('1'))
+  const amountB = new TokenAmount(token0, parseEther('0.5'))
+
+  const params = getBidAskDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionX = params.distributionX.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+  const sumDistributionY = params.distributionY.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+  expect(params.distributionX).toEqual([
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt('24390243902439024'),
+    BigInt('97560975609756097'),
+    BigInt('146341463414634146'),
+    BigInt('195121951219512195'),
+    BigInt('243902439024390243'),
+    BigInt('292682926829268292')
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt('292682926829268292'),
+    BigInt('243902439024390243'),
+    BigInt('195121951219512195'),
+    BigInt('146341463414634146'),
+    BigInt('97560975609756097'),
+    BigInt('24390243902439024'),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0)
+  ])
+  expect(sumDistributionX).toBeLessThan(BigInt(10) ** BigInt(18) - BigInt(1))
+  expect(sumDistributionY).toBeLessThan(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getBidAskDistributionFromBinRange with amount X only', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('1'))
+  const amountB = new TokenAmount(token0, parseEther('0'))
+
+  const params = getBidAskDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionX = params.distributionX.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([0, 1, 2, 3, 4, 5])
+  expect(params.distributionX).toEqual([
+    BigInt('47619047619047619'),
+    BigInt('95238095238095238'),
+    BigInt('142857142857142857'),
+    BigInt('190476190476190476'),
+    BigInt('238095238095238095'),
+    BigInt('285714285714285714')
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0)
+  ])
+  expect(sumDistributionX).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getBidAskDistributionFromBinRange with amount Y only', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('0'))
+  const amountB = new TokenAmount(token0, parseEther('0.5'))
+
+  const params = getBidAskDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionY = params.distributionY.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([-5, -4, -3, -2, -1, 0])
+  expect(params.distributionX).toEqual([
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0)
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt('285714285714285714'),
+    BigInt('238095238095238095'),
+    BigInt('190476190476190476'),
+    BigInt('142857142857142857'),
+    BigInt('95238095238095238'),
+    BigInt('47619047619047619')
+  ])
+  expect(sumDistributionY).toEqual(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getCurveDistributionFromBinRange with amount X and Y', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('1'))
+  const amountB = new TokenAmount(token0, parseEther('0.5'))
+
+  const params = getCurveDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionX = params.distributionX.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+  const sumDistributionY = params.distributionY.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+  expect(params.distributionX).toEqual([
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt('139235091095125611'),
+    BigInt('264309887127866457'),
+    BigInt('226004736020696260'),
+    BigInt('174096921395845034'),
+    BigInt('120818673548873833'),
+    BigInt('75534690811592802')
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt('75534690811592802'),
+    BigInt('120818673548873833'),
+    BigInt('174096921395845034'),
+    BigInt('226004736020696260'),
+    BigInt('264309887127866457'),
+    BigInt('139235091095125611'),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0),
+    BigInt(0)
+  ])
+  expect(sumDistributionX).toBeLessThan(BigInt(10) ** BigInt(18) - BigInt(1))
+  expect(sumDistributionY).toBeLessThan(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getCurveDistributionFromBinRange with amount X only', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('1'))
+  const amountB = new TokenAmount(token0, parseEther('0'))
+
+  const params = getCurveDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionX = params.distributionX.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([0, 1, 2, 3, 4, 5])
+  expect(params.distributionX).toEqual([
+    BigInt('244436099596057025'),
+    BigInt('232006448180761579'),
+    BigInt('198382877939128516'),
+    BigInt('152819135187004190'),
+    BigInt('106052450888545820'),
+    BigInt('66302988208502866')
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0')
+  ])
+  expect(sumDistributionX).toBeLessThan(BigInt(10) ** BigInt(18) - BigInt(1))
+})
+
+it('getCurveDistributionFromBinRange with amount Y only', () => {
+  const activeId = 8376038
+  const amountA = CurrencyAmount.ether(43113, parseEther('0'))
+  const amountB = new TokenAmount(token0, parseEther('0.5'))
+
+  const params = getCurveDistributionFromBinRange(
+    activeId,
+    [activeId - 5, activeId + 5],
+    [amountA, amountB]
+  )
+
+  const sumDistributionY = params.distributionY.reduce(
+    (a, b) => a + b,
+    BigInt(0)
+  )
+
+  expect(params.deltaIds).toEqual([-5, -4, -3, -2, -1, 0])
+  expect(params.distributionX).toEqual([
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0'),
+    BigInt('0')
+  ])
+  expect(params.distributionY).toEqual([
+    BigInt('66302988208502866'),
+    BigInt('106052450888545820'),
+    BigInt('152819135187004190'),
+    BigInt('198382877939128516'),
+    BigInt('232006448180761579'),
+    BigInt('244436099596057025')
+  ])
+  expect(sumDistributionY).toBeLessThan(BigInt(10) ** BigInt(18) - BigInt(1))
 })
--- packages/v2/src/utils/liquidityDistribution.ts
+++ packages/v2/src/utils/liquidityDistribution.ts
@@ -133,89 +133,65 @@ export const getBidAskDistributionFromBinRange = (
 ): LiquidityDistributionParams => {
   const [parsedAmountA, parsedAmountB] = parsedAmounts
 
-  // init return values
-  let deltaIds: number[] = [],
-    _distributionX: number[] = [],
-    _distributionY: number[] = []
+  const deltaIds: number[] = []
+  const distributionX: bigint[] = []
+  const distributionY: bigint[] = []
 
-  // range only includes B tokens (Y tokens)
-  if (binRange[1] <= activeId && parsedAmountA.raw.toString() === '0') {
-    const negDelta = binRange[1] - binRange[0] + 1
-    const negativeDeltaIds = Array.from(Array(activeId - binRange[0]).keys())
-      .reverse()
-      .slice(0, negDelta)
-      .map((el) => -1 * (el + 1))
+  let nb_x = 0
+  let nb_y = 0
+  for (let binId = binRange[0]; binId <= binRange[1]; binId++) {
+    const weight = Math.abs(binId - activeId) + 1
 
-    deltaIds = [...negativeDeltaIds]
-    if (activeId === binRange[1]) {
-      deltaIds.push(0)
+    if (binId >= activeId) {
+      nb_x += 2 * weight
+    }
+    if (binId <= activeId) {
+      nb_y += 2 * weight
+    }
+    if (binId === activeId) {
+      if (parsedAmountB.greaterThan('0')) {
+        nb_x -= weight
+      }
+      if (parsedAmountA.greaterThan('0')) {
+        nb_y -= weight
+      }
+    }
+  }
+
+  for (let binId = binRange[0]; binId <= binRange[1]; binId++) {
+    let dist_x = BigInt(0)
+    let dist_y = BigInt(0)
+
+    const weight = parseEther(`${Math.abs(binId - activeId) + 1}`)
+
+    if (binId >= activeId && parsedAmountA.greaterThan('0')) {
+      dist_x = (BigInt(2) * weight) / BigInt(nb_x)
     }
 
-    _distributionX = [...Array(deltaIds.length).fill(0)]
-
-    // dist = 2/R^2 * r
-    const rSquare = Math.pow(deltaIds[0], 2)
-    _distributionY = deltaIds.map((i) => ((i - 1) * -2) / rSquare)
-  }
-
-  // range only includes A tokens (X tokens)
-  else if (activeId <= binRange[0] && parsedAmountB.raw.toString() === '0') {
-    const posDelta = binRange[1] - binRange[0] + 1
-    const positiveDeltaIds = Array.from(Array(binRange[1] - activeId).keys())
-      .reverse()
-      .slice(0, posDelta)
-      .reverse()
-      .map((el) => el + 1)
-
-    deltaIds = [...positiveDeltaIds]
-    if (activeId === binRange[0]) {
-      deltaIds.unshift(0)
+    if (binId <= activeId && parsedAmountB.greaterThan('0')) {
+      dist_y = (BigInt(2) * weight) / BigInt(nb_y)
     }
-    // dist = 2/R^2 * i
-    const rSquare = Math.pow(deltaIds[deltaIds.length - 1], 2)
-    _distributionX = deltaIds.map((i) => ((i + 1) * 2) / rSquare)
-    _distributionY = [...Array(deltaIds.length).fill(0)]
+
+    if (
+      binId === activeId &&
+      parsedAmountA.greaterThan('0') &&
+      parsedAmountB.greaterThan('0')
+    ) {
+      dist_x /= BigInt(2)
+      dist_y /= BigInt(2)
+    }
+
+    if (dist_x > 0 || dist_y > 0) {
+      distributionX.push(dist_x)
+      distributionY.push(dist_y)
+      deltaIds.push(binId - activeId)
+    }
   }
 
-  // range includes both X and Y tokens
-  else {
-    const negDelta = activeId - binRange[0]
-    const posDelta = binRange[1] - activeId
-
-    const negativeDeltaIds = Array.from(Array(negDelta).keys())
-      .reverse()
-      .map((el) => -1 * (el + 1))
-    const positiveDeltaIds = Array.from(Array(posDelta).keys()).map(
-      (el) => el + 1
-    )
-    deltaIds = [...negativeDeltaIds, 0, ...positiveDeltaIds]
-
-    // dist = 1/R^2 * i
-    const rSquareX = Math.pow(positiveDeltaIds[positiveDeltaIds.length - 1], 2)
-    _distributionX = [
-      ...Array(negDelta).fill(0),
-      1 / rSquareX,
-      ...positiveDeltaIds.map((i) => (i + 1) / rSquareX)
-    ]
-
-    // dist = 1/R^2 * i
-    const rSquareY = Math.pow(negativeDeltaIds[0], 2)
-    _distributionY = [
-      ...negativeDeltaIds.map((i) => (-1 * (i - 1)) / rSquareY),
-      1 / rSquareY,
-      ...Array(posDelta).fill(0)
-    ]
-  }
-
-  // return
   return {
     deltaIds,
-    distributionX: _distributionX.map((el) =>
-      parseDistributionValue(el, parsedAmountA.currency.decimals)
-    ),
-    distributionY: _distributionY.map((el) =>
-      parseDistributionValue(el, parsedAmountB.currency.decimals)
-    )
+    distributionX,
+    distributionY
   }
 }
 
@@ -234,149 +210,77 @@ export const getCurveDistributionFromBinRange = (
 ): LiquidityDistributionParams => {
   const [parsedAmountA, parsedAmountB] = parsedAmounts
 
-  // init return values
-  let deltaIds: number[] = [],
-    _distributionX: number[] = [],
-    _distributionY: number[] = []
+  const ONE = BigInt(10) ** BigInt(18)
 
-  // get sigma based on radius R
-  const getSigma = (_R: number) => {
-    const factor =
-      _R >= 20
-        ? 2.0
-        : _R >= 15
-        ? 1.8
-        : _R >= 10
-        ? 1.7
-        : _R >= 8
-        ? 1.6
-        : _R >= 6
-        ? 1.5
-        : _R >= 5
-        ? 1.4
-        : 1.0
-    return _R / factor
+  const deltaIds: number[] = []
+  const distributionX: bigint[] = []
+  const distributionY: bigint[] = []
+
+  const getGaussianDistribution = (x: number, sigma: number): bigint => {
+    return parseEther(`${Math.exp(-((x / sigma) ** 2) / 2)}`)
   }
 
-  // range only includes B tokens (Y tokens)
-  if (binRange[1] <= activeId && parsedAmountA.raw.toString() === '0') {
-    const negDelta = binRange[1] - binRange[0] + 1
-    const negativeDeltaIds = Array.from(Array(activeId - binRange[0]).keys())
-      .reverse()
-      .slice(0, negDelta)
-      .map((el) => -1 * (el + 1))
+  const getSigma = (radius: number): number => {
+    return radius / Math.sqrt(1 + Math.log(radius))
+  }
 
-    deltaIds = [...negativeDeltaIds]
-    if (activeId === binRange[1]) {
-      deltaIds.push(0)
+  const radius_x = Math.abs(binRange[0] - activeId)
+  const radius_y = Math.abs(binRange[1] - activeId)
+
+  const sigma_x = getSigma(radius_x)
+  const sigma_y = getSigma(radius_y)
+
+  let nb_x = BigInt(0)
+  let nb_y = BigInt(0)
+
+  for (let binId = binRange[0]; binId <= binRange[1]; binId++) {
+    const deltaId = binId - activeId
+    let dist_x = BigInt(0)
+    let dist_y = BigInt(0)
+
+    if (deltaId >= 0 && parsedAmountA.greaterThan('0')) {
+      dist_x = BigInt(2) * getGaussianDistribution(deltaId, sigma_x)
     }
 
-    _distributionX = [...Array(deltaIds.length).fill(0)]
-
-    // radius is num of bins
-    const R = deltaIds.length - 1
-    const sigma = getSigma(R)
-
-    // A = 1 / (sigma  * sqrt(2 * pi))
-    const A = 1 / (Math.sqrt(Math.PI * 2) * sigma)
-
-    // dist = 2 * A * exp(-0.5 * (r /sigma) ^ 2)
-    // r is distance from right-most bin
-    _distributionY = deltaIds.map(
-      (_, ind) => 2 * A * Math.exp(-0.5 * Math.pow((R - ind) / sigma, 2))
-    )
-  }
-
-  // range only includes A tokens (X tokens)
-  else if (activeId <= binRange[0] && parsedAmountB.raw.toString() === '0') {
-    const posDelta = binRange[1] - binRange[0] + 1
-    const positiveDeltaIds = Array.from(Array(binRange[1] - activeId).keys())
-      .reverse()
-      .slice(0, posDelta)
-      .reverse()
-      .map((el) => el + 1)
-
-    deltaIds = [...positiveDeltaIds]
-    if (activeId === binRange[0]) {
-      deltaIds.unshift(0)
+    if (deltaId <= 0 && parsedAmountB.greaterThan('0')) {
+      dist_y = BigInt(2) * getGaussianDistribution(deltaId, sigma_y)
     }
 
-    _distributionY = [...Array(deltaIds.length).fill(0)]
+    if (
+      deltaId === 0 &&
+      parsedAmountA.greaterThan('0') &&
+      parsedAmountB.greaterThan('0')
+    ) {
+      dist_x /= BigInt(2)
+      dist_y /= BigInt(2)
+    }
 
-    // radius is num of bins
-    const R = deltaIds.length - 1
-    const sigma = getSigma(R)
+    nb_x += dist_x
+    nb_y += dist_y
 
-    // A = 1 / (sigma  * sqrt(2 * pi))
-    const A = 1 / (Math.sqrt(Math.PI * 2) * sigma)
-
-    // dist = 2 * A * exp(-0.5 * (r /sigma) ^ 2)
-    // r is distance from left-most bin
-    _distributionX = deltaIds.map(
-      (_, ind) => 2 * A * Math.exp(-0.5 * Math.pow(ind / sigma, 2))
-    )
+    if (dist_x > 0 || dist_y > 0) {
+      distributionX.push(dist_x)
+      distributionY.push(dist_y)
+      deltaIds.push(deltaId)
+    }
   }
 
-  // range includes both X and Y tokens
-  else {
-    const negDelta = activeId - binRange[0]
-    const posDelta = binRange[1] - activeId
-
-    const negativeDeltaIds = Array.from(Array(negDelta).keys())
-      .reverse()
-      .map((el) => -1 * (el + 1))
-    const positiveDeltaIds = Array.from(Array(posDelta).keys()).map(
-      (el) => el + 1
-    )
-    deltaIds = [...negativeDeltaIds, 0, ...positiveDeltaIds]
-
-    // radius is num of bins
-    const RX = positiveDeltaIds.length
-    const sigmaX = getSigma(RX)
-
-    // A = 1 / (sigma  * sqrt(2 * pi))
-    const AX = 1 / (Math.sqrt(Math.PI * 2) * sigmaX)
-
-    // dist = 2 * A * exp(-0.5 * (r /sigma) ^ 2)
-    // r is distance from 0
-    _distributionX = [
-      ...Array(negDelta).fill(0),
-      AX,
-      ...positiveDeltaIds.map(
-        (_, ind) => 2 * AX * Math.exp(-0.5 * Math.pow((ind + 1) / sigmaX, 2))
-      )
-    ]
-
-    // radius is num of bins
-    const RY = negativeDeltaIds.length
-    const sigmaY = getSigma(RY)
-
-    // A = 1 / (sigma  * sqrt(2 * pi))
-    const AY = 1 / (Math.sqrt(Math.PI * 2) * sigmaY)
-
-    // dist = 2 * A * exp(-0.5 * (r /sigma) ^ 2)
-    // r is distance from 0
-    _distributionY = [
-      ...negativeDeltaIds.map(
-        (_, ind) => 2 * AY * Math.exp(-0.5 * Math.pow((RY - ind) / sigmaY, 2))
-      ),
-      AY,
-      ...Array(posDelta).fill(0)
-    ]
+  for (let i = 0; i < distributionX.length; i++) {
+    if (nb_x > 0) {
+      distributionX[i] = (BigInt(distributionX[i]) * ONE) / BigInt(nb_x)
+    } else {
+      distributionX[i] = BigInt(0)
+    }
+    if (nb_y > 0) {
+      distributionY[i] = (BigInt(distributionY[i]) * ONE) / BigInt(nb_y)
+    } else {
+      distributionY[i] = BigInt(0)
+    }
   }
 
-  // return
   return {
     deltaIds,
-    distributionX: _distributionX.map((el) =>
-      parseDistributionValue(el, parsedAmountA.currency.decimals)
-    ),
-    distributionY: _distributionY.map((el) =>
-      parseDistributionValue(el, parsedAmountB.currency.decimals)
-    )
+    distributionX,
+    distributionY
   }
-}
-
-const parseDistributionValue = (value: number, decimals: number) => {
-  return parseEther(`${parseFloat(value.toFixed(decimals))}`)
 }
