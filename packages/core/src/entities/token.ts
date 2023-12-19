9ab3159ff9e102c6791b082641bb524d71ecd9a5
Maxime <52084503+Max-3-7@users.noreply.github.com>
2023-12-19 10:37:18 +0100

    add mantle chain id (#30)
    * add mantle
    
    * add changeset

--- packages/core/src/entities/token.ts
+++ packages/core/src/entities/token.ts
@@ -125,6 +125,13 @@ export const WNATIVE = {
     18,
     'WETH',
     'Wrapped ETH'
+  ),
+  [ChainId.MANTLE]: new Token(
+    ChainId.MANTLE,
+    '0x78c1b0C915c4FAA5FffA6CAbf0219DA63d7f4cb8',
+    18,
+    'WMNT',
+    'Wrapped Mantle'
   )
 }
 
